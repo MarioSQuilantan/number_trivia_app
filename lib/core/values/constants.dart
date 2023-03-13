354e4acb6a5b0debcca8e49456cf93f55daec54a
MarioSQuilantan <mario.s@enhancefitness.com>
2023-03-13 15:52:55 -0600

    refactor: :zap: updated constants file

--- lib/core/values/constants.dart
+++ lib/core/values/constants.dart
@@ -1,11 +1,22 @@
 // routes
+import 'package:flutter/material.dart';
+
 class RouteConstants {
   static String home = '/';
 }
 
 class ApiConstants {
-  static const String _baseUrl = 'http://numbersapi.com/';
+  static const String _baseUrl = 'http://numbersapi.com';
   static const Duration connectionTimeout = Duration(milliseconds: 15000);
 
   static String get getRandomNumberTrivia => '$_baseUrl/random/trivia?json';
+}
+
+class HomePhoneLayoutKeys {
+  static const Key getRandomNumberTrivia = Key('get_random_number_trivia');
+  static const Key numberTriviaData = Key('number_trivia_data');
+}
+
+class Routes {
+  static const String home = '/';
 }
